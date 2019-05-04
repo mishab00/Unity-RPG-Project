01b5e98351bffead4a2023510ca2993d8dca9818
mishab00 <bmisha00@gmail.com>
2019-05-04 18:27:17 +0300

    Adding health to enemy

--- RPG Project/Assets/Scripts/Combat/Fighter.cs
+++ RPG Project/Assets/Scripts/Combat/Fighter.cs
@@ -6,11 +6,16 @@ namespace RPG.Combat
     public class Fighter : MonoBehaviour,IAction
     {
         [SerializeField] float weaponRange = 2f;
+        [SerializeField] float timeBetweenAttacks = 1f;
+        [SerializeField] float damage = 5f;
 
         Transform target;
+        float timeSinceLastAttack = 0;
 
         private void Update()
         {
+            timeSinceLastAttack += Time.deltaTime;
+
             if(target == null) return;
 
             if (!GetIsInRange())
@@ -20,6 +25,17 @@ namespace RPG.Combat
             else
             {
                 GetComponent<Mover>().Cancel();
+                AttackBehaviour();
+            }
+        }
+
+        private void AttackBehaviour()
+        {
+            if(timeSinceLastAttack > timeBetweenAttacks) {
+                GetComponent<Animator>().SetTrigger("attack");
+                timeSinceLastAttack = 0;
+                target.GetComponent<Health>().TakeDamage(damage);
+
             }
         }
 
@@ -35,6 +51,10 @@ namespace RPG.Combat
 
         public void Cancel() {
             target = null;
+        }
+
+        // Animation Event
+        void Hit() {
         }
     }
 }
