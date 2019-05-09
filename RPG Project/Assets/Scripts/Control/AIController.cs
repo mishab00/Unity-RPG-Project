291a7f9760b67aa37db3b0968ee2df166be11ef5
mishab00 <bmisha00@gmail.com>
2019-05-09 22:12:14 +0300

    add suspicion position

--- RPG Project/Assets/Scripts/Control/AIController.cs
+++ RPG Project/Assets/Scripts/Control/AIController.cs
@@ -1,38 +1,70 @@
 using UnityEngine;
 using RPG.Combat;
 using RPG.Core;
+using RPG.Movement;
 namespace RPG.Control
 {
     public class AIController : MonoBehaviour
     {
         [SerializeField] float chaseDistance = 5f;
+        [SerializeField] float suspicionTime = 3f;
 
         Fighter fighter;
         Health health;
         GameObject player;
+        Mover mover;
+
+        Vector3 guardPosition;
+        float timeSinceLastSawPlayer = Mathf.Infinity;
 
         private void Start() {
             player = GameObject.FindWithTag("Player");
             fighter = GetComponent<Fighter>();
             health = GetComponent<Health>();
+            mover = GetComponent<Mover>();
+
+            guardPosition = transform.position;
         }
 
         private void Update()
         {
             if(health.IsDead()) return;
 
-            if(InAttackRangeOfPlayer() && CanAttack()) {
-                print(gameObject.name + " is in chase radius");
-                fighter.Attack(player.gameObject);
-
-            } else {
-                fighter.Cancel();
+            if(InAttackRangeOfPlayer() && CanAttack())
+            {
+                timeSinceLastSawPlayer = 0;
+                AttackBehaviour();
             }
+            else if(timeSinceLastSawPlayer < suspicionTime)
+            {
+                SuspicionBehaviour();
+            }
+            else
+            {
+                GuardBehaviour();
+            }
+            timeSinceLastSawPlayer += Time.deltaTime;
+
+        }
+
+        private void SuspicionBehaviour()
+        {
+            GetComponent<ActionScheduler>().CancelCurrentAction();
+        }
+
+        private void GuardBehaviour()
+        {
+            fighter.Cancel();
+            mover.StartMoveAction(guardPosition);
+        }
+
+        private void AttackBehaviour()
+        {
+            fighter.Attack(player.gameObject);
         }
 
         private bool CanAttack() {
             bool playerIsValid = fighter.CanAttack(player.gameObject);
-            //bool enemyIsValid = fighter.CanAttack(gameObject);
             return playerIsValid;
         }
 
@@ -40,6 +72,12 @@ namespace RPG.Control
         {
             float distanceToPlayer = Vector3.Distance(transform.position, player.transform.position);
             return distanceToPlayer <= chaseDistance; 
+        }
+
+        //called by unity
+        private void OnDrawGizmosSelected() {
+            Gizmos.color = Color.blue;
+            Gizmos.DrawWireSphere(transform.position, chaseDistance);
         }
     }
 }
